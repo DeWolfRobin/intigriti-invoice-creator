d756b9e687fd6a69fbc12d7f87a8ca97c99e6938
Robin De Wolf <rdewolf@telenet.be>
2019-03-20 22:11:25 +0100

    small fix

--- get_payouts.sh
+++ get_payouts.sh
@@ -1,7 +1,7 @@
 username=""
 password=""
 data="{\"email\":\"$username\",\"password\":\"$password\",\"returnUrl\":\"\"}"
-json=$(curl -X 'POST' 'https://www.intigriti.com/authentication/login' -H 'Content-Type: application/json;charset=utf-8' --data-binary $data)
+json=$(curl -s -X 'POST' 'https://www.intigriti.com/authentication/login' -H 'Content-Type: application/json;charset=utf-8' --data-binary $data)
 token=`echo $json | jq -r ".token"`
-output=`curl 'https://api.intigriti.com/api/payout' -H "Authorization: Bearer $token"`
+output=`curl -s 'https://api.intigriti.com/api/payout' -H "Authorization: Bearer $token"`
 echo $output
